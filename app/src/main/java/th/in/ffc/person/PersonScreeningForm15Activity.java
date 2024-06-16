aa8a92e974a6e187c7ead97a29ed6bd0059d5486
tanatips <tanatip_s@hotmail.com>
2024-06-16 21:28:57 +0700

    support screening_form_15_35

---
+++ app/src/main/java/th/in/ffc/person/PersonScreeningForm15Activity.java
@@ -0,0 +1,17 @@
+package th.in.ffc.person;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import th.in.ffc.R;
+
+public class PersonScreeningForm15Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_screening_form15);
+
+    }
+}
---
+++ app/src/main/java/th/in/ffc/person/PersonScreeningForm35Activity.java
@@ -0,0 +1,21 @@
+package th.in.ffc.person;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import th.in.ffc.R;
+
+public class PersonScreeningForm35Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_screening_form35);
+
+    }
+}
